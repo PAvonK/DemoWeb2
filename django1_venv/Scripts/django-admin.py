102daadfed0ab3a93a76a4a1955552c0c61b7be9
Fidranth <pavk13@outlook.com>
2020-05-12 23:50:57 -0600

    Initial virtual environment files

---
+++ django1_venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\programming_master\projects\demoweb2\django1_venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
