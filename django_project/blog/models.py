c775c08b1205f24148e05949861e78415b529d66
Fidranth <pavk13@outlook.com>
2020-05-13 18:57:57 -0600

    Updated upon Database Migrations Video pt 5

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User # to import the user from user table
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+        # the models.DateTimeField could be set to (auto_now=True) which updates the time for when the post is updated, it can also be (auto_now_add=True) which means it only posts the time of the original post but cannot be changed.  instead we import timezone and do the above so we can change the time if need be manually
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # ForeignKey passes in the other table with the argument User as well in this case a command on_delete which states if the user is deleted = models.CASCADE whih means thier posts get deleted also. 
+
+    def __str__(self):
+        return self.title
+
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,27 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 from django.http import HttpResponse
-
-# Dummy data / normally this would come from database
-posts = [
-    {
-        'author': 'Phil von Kaenel',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Tiesha von Kaenel',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2019'
-    }
-]
 
 # this is the logic for how we want to handle when a user wants to go to our home page
 def home(request):
     # dictionary key / value to connect to posts from above
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     # render is how we return that template 
     return render(request, 'blog/home.html', context)
